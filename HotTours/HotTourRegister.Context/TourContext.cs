acaaab18f6ae43b0a1af022a4dab6a95fb4aea4b
l4mberjack <l4mberjack@mail.ru>
2025-12-10 19:51:21 +0300

    байден бласт

---
+++ HotTours/HotTourRegister.Context/TourContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotTourRegister.Context
+{
+    /// <summary>
+    /// Контекст базы данных туров
+    /// </summary>
+    public class TourContext : DbContext
+    {
+        public DbSet<Tour> Tours { get; set; } = null!;
+        public TourContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            }
+        }
+    }
+}
---
+++ HotTours/Repository/TourRepository.cs
@@ -0,0 +1,59 @@
+﻿using Entities;
+using HotTourRegister.Context;
+using Repository.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    /// <summary>
+    /// Репозиторий для доступа к турам
+    /// </summary>
+    public class TourRepository(TourContext context) : IStorage
+    {
+        public async Task Add(Tour tour, CancellationToken token)
+        {
+            context.Add(tour);
+            await context.SaveChangesAsync(token);
+        }
+        public async Task Delete(Guid id, CancellationToken token)
+        {
+            context.Remove(id);
+            await context.SaveChangesAsync(token);
+        }
+        public async Task<ICollection<Tour>> GetAll(CancellationToken token)
+        {
+            return await context.Set<Tour>().ToListAsync(token);
+        }
+        public async Task<Tour?> GetById(Guid id, CancellationToken token)
+        {
+            return await context.Set<Tour>().FirstOrDefaultAsync(x => x.Id == id, token);
+        }
+        public async Task<TourStatistics> GetStatistics(CancellationToken token)
+        {
+            var tourSet = context.Set<Tour>();
+
+            var tourCount = await tourSet.CountAsync(token);
+            var totalPriceAllTours = await tourSet
+                .SumAsync(x => x.PricePerPerson * x.TouristCount + x.ExtraCharges, token);
+            var tourCountCharge = await tourSet
+                .CountAsync(x => x.ExtraCharges > 0, token);
+            var tourSumCharge = await tourSet
+                .SumAsync(x => x.ExtraCharges, token);
+
+            var statistics = new TourStatistics
+            {
+                TourCount = tourCount,
+                TotalPriceAllTours = totalPriceAllTours,
+                TourCountCharge = tourCountCharge,
+                TourSumCharge = tourSumCharge
+            };
+
+            return statistics;
+        }
+        public async Task Update(Tour tour, CancellationToken token)
+        {
+            context.Update(tour);
+            await context.SaveChangesAsync(token);
+        }
+    }
+}
